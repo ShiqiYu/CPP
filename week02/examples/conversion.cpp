c326aa0f58bce32643c6107b20d9babc231468a6
Shiqi Yu <shiqi.yu@gmail.com>
2021-09-12 21:28:45 +0800

    chapter 2 update

--- week02/examples/conversion.cpp
+++ week02/examples/conversion.cpp
@@ -11,14 +11,6 @@ int main()
     float num_float = 2.3; // implicit conversion from double to float, may loss precision
     short num_short = 650000; 
 
-    int num_int6 = 0x7ABCDEF0;
-    float num_int_float = num_int6;
-    int num_int7 = (int)(num_int_float);
-    cout << "num_int6 = " << num_int6 << endl;
-    cout << "num_int7 = " << num_int7 << endl;
-
-    cout << "num_int5 = " << num_int5 << endl;
-    cout << "num_float = " << num_float << endl;
     cout << "num_short = " << num_short << endl;
 
     return 0;
--- week02/examples/overflow.cpp
+++ week02/examples/overflow.cpp
@@ -8,7 +8,7 @@ int main()
     int c = a * b;
     cout << "c = " << c << endl;
 
-    unsigned int c1 = a * b; //danger operation
-    cout << "c1 = " << c1 << endl;
+    // unsigned int c1 = a * b; //danger operation
+    // cout << "c1 = " << c1 << endl;
     return 0;
 }
