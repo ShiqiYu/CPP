881b86aa73fc76e667bce8846466c489c4e3ae21
git <shiqi.yu@gmail.com>
2024-05-10 20:58:44 +0800

    update week11

--- week11/examples/example2/main.cpp
+++ week11/examples/example2/main.cpp
@@ -3,19 +3,20 @@
 
 using namespace std;
 
-// Why memory leak and memory double free?
 int main()
 {
-    MyString str1(10, "Shenzhen");
-    cout << "str1: " << str1 << endl;
+    {
+        MyString str1(10, "Shenzhen");
+        cout << "str1: " << str1 << endl;
 
-    MyString str2 = str1; 
-    cout << "str2: " << str2 << endl;
+        MyString str2 = str1; 
+        cout << "str2: " << str2 << endl;
 
-    MyString str3;
-    cout << "str3: " << str3 << endl;
-    str3 = str1;
-    cout << "str3: " << str3 << endl;
-
+        MyString str3;
+        cout << "str3: " << str3 << endl;
+        str3 = str1;
+        cout << "str3: " << str3 << endl;
+    }
+    cout << "end of main()" << endl; // it can be printed out
     return 0;
 }
