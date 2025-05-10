4cd76d830836910e1305c78d488c1e7100391b81
Simranjit kaur <amatjitsohal769@gmail.com>
2025-05-10 11:50:45 +0530

    added some comments and changed some code

--- week03/examples/while.cpp
+++ week03/examples/while.cpp
@@ -3,14 +3,15 @@ using namespace std;
 int main()
 {
     int num = 10;
-    while(num > 0)
+    while(num > 0) // when num gets 0 loop will break
     {
         cout << "num = " << num << endl;
         num--;
     }
-
+    // num gets 0 and priting it out of while loop
+    cout<<"num = "<<num;
     // num = 10;
-    // do
+    // do 
     // {
     //     cout << "num = " << num << endl;
     //     num--;
--- week04/examples/array.cpp
+++ week04/examples/array.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int num_array1[5]; //uninitialized array, random values 
+    int num_array1[5]; // 5 is the size of the array and array is uninitialized it will give random values 
     int num_array2[5] = {0, 1, 2, 3, 4}; //initialization
 
     for(int idx = 0; idx < 5; idx++)
